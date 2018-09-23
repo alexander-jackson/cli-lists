e461603903270f191a59443baa186eb0387fc194
Alexander Jackson <alexanderjackson@protonmail.com>
2018-09-23 15:58:08 +0100

    Updated to display the lists and free any memory used by the process.

--- src/main.c
+++ src/main.c
@@ -9,6 +9,11 @@ int main(void) {
 
 	struct List** lists = parse_file(contents);
 
+	for (size_t i = 0; lists[i] != NULL; ++i) {
+		// Display the list
+		display_list(lists[i]);
+	}
+
 	// Ensure we free the lists
 	for (size_t i = 0; lists[i] != NULL; ++i) {
 		free_list(lists[i]);
