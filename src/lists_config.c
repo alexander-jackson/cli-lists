843496923860d560cb77b901f12e2828c85ea31b
Alexander Jackson <alexanderjackson@protonmail.com>
2018-09-26 09:07:34 +0100

    Added a config file effectively which currently only contains a definition for where the user's TOML file should be stored.

---
+++ src/lists_config.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+#include "lists_config.h"
+
+char* DEFAULT_FILEPATH = "lists.toml";
---
+++ src/lists_config.h
@@ -0,0 +1,6 @@
+#ifndef LISTS_CONFIG_H
+#define LISTS_CONFIG_H
+
+extern char* DEFAULT_FILEPATH;
+
+#endif
