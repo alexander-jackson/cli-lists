43be7bbdefbb0fce48f2d47e7992a57b179cbdfc
Alexander Jackson <alexanderjackson@protonmail.com>
2018-09-26 09:05:10 +0100

    Added testing for the add_list method and also commented out the other functionality tests.

--- src/main.c
+++ src/main.c
@@ -4,25 +4,41 @@
 #include "file_io.h"
 #include "parser.h"
 #include "list.h"
+#include "controller.h"
 
-int main(void) {
-	char* contents = read_file("sample.toml");
+int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		// Display the help menu
+		puts("Help");
 
-	struct List** lists = parse_file(contents);
-
-	for (size_t i = 0; lists[i] != NULL; ++i) {
-		// Display the list
-		display_list(lists[i]);
+		return 0;
 	}
 
-	// Ensure we free the lists
-	for (size_t i = 0; lists[i] != NULL; ++i) {
-		free_list(lists[i]);
-	}
+	// Get the second argument
+	char* command = argv[1];
 
-	free(lists);
+	printf("Command: %s\n", command);
 
-	free(contents);
+	// Run it
+	run_command(command, argc, argv);
+
+	//char* contents = read_file("sample.toml");
+
+	//struct List** lists = parse_file(contents);
+
+	//for (size_t i = 0; lists[i] != NULL; ++i) {
+	//	// Display the list
+	//	display_list(lists[i]);
+	//}
+
+	//// Ensure we free the lists
+	//for (size_t i = 0; lists[i] != NULL; ++i) {
+	//	free_list(lists[i]);
+	//}
+
+	//free(lists);
+
+	//free(contents);
 
 	return 0;
 }
