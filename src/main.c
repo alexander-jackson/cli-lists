5889ad901cda22c59f7caab3e7971b7fdd838f4f
Alexander Jackson <alexanderjackson@protonmail.com>
2018-09-27 18:58:51 +0100

    Removed some commented code.

--- src/main.c
+++ src/main.c
@@ -17,28 +17,8 @@ int main(int argc, char* argv[]) {
 	// Get the second argument
 	char* command = argv[1];
 
-	printf("Command: %s\n", command);
-
 	// Run it
 	run_command(command, argc, argv);
-
-	//char* contents = read_file("sample.toml");
-
-	//struct List** lists = parse_file(contents);
-
-	//for (size_t i = 0; lists[i] != NULL; ++i) {
-	//	// Display the list
-	//	display_list(lists[i]);
-	//}
-
-	//// Ensure we free the lists
-	//for (size_t i = 0; lists[i] != NULL; ++i) {
-	//	free_list(lists[i]);
-	//}
-
-	//free(lists);
-
-	//free(contents);
 
 	return 0;
 }
